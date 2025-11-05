ae3bc0abb8fc0c98e8fee961884c5da975f827a6
SamudrapuVenkataSai <venkatasaisamudrapu@gmail.com>
2025-11-05 10:01:21 +0530

    Create Hashmap.java

---
+++ Day_6/Hashmap.java
@@ -0,0 +1,18 @@
+// Write a java program to remove an element from the Map, you can use the remove() method. This method takes the key value and
+// removes the mapping for a key from this map if it is present in the map.
+
+// Instruction- before running the program , give 4 string inputs and then the one  integer number which key you want to remove.
+// keys should start from 1.
+
+import java.util.Scanner;
+// Java program to remove
+// elements from HashMap
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+        //write your answer here
+    }
+}
